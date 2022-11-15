058a465735d2be5a12eba38372ac9301a7faa84d
Jacob Phillips <jakephillips.wv@gmail.com>
2022-11-15 17:52:38 -0600

    made strings in model nullable

--- ExerciseDBAPI/Exercise.cs
+++ ExerciseDBAPI/Exercise.cs
@@ -8,11 +8,11 @@ namespace ExerciseDBAPI
 {
     public class Exercise
     {
-        public string BodyPart { get; set; }
-        public string Equipment { get; set; }
-        public string GifUrl { get; set; }
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public string Target { get; set; }
+        public string? BodyPart { get; set; }
+        public string? Equipment { get; set; }
+        public string? GifUrl { get; set; }
+        public string? ID { get; set; }
+        public string? Name { get; set; }
+        public string? Target { get; set; }
     }
 }
